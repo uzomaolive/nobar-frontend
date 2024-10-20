45fac38d3a242002e1b334bacb474793ef946bec
Olive Uzoma <olive.c.uzoma@gmail.com>
2024-10-20 22:12:15 +0100

    feat: add plugin header for NoBar Frontend

---
+++ nobar-frontend.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: NoBar Frontend
+ * Description: A lightweight plugin that hides the admin bar from the frontend for logged-in users.
+ * Version: 1.0.0
+ * Author: Olive Uzoma
+ * Author URI: https://oliveuzoma.com
+ */
